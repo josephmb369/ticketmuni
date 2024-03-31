194cda7707628e23ae14a68a5bb603b0106513ff
JHONATHAN-9507 <jhonatan.perea@innovating.com.co>
2024-03-30 19:03:21 -0500

    buscador_funcional

--- lib/config.php
+++ lib/config.php
@@ -1,7 +1,16 @@
 <?php
 
 define("USER", "root");
-define("SERVER", "localhost:33065");
+define("SERVER", "localhost:3306");
 define("BD", "ticket");
 define("PASS", "");
+
+$conn = new mysqli(SERVER, USER, PASS, BD);
+
+// Verificar si hay errores de conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+
+
 ?>
